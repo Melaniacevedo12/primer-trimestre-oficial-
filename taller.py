ec3dd14624e797d4f9137ed995c5a0934bc13928
Melani Sofia <mel1251343@gmail.com>
2025-05-31 16:06:16 -0500

    actualizacion

--- taller.py
+++ taller.py
@@ -1,5 +1,7 @@
 print("------------------------------------------------------------------------")
+
 print("********************************* TALLER 1 ********************************")
+
 nombre=input("Cual es el nombre del producto: ")
 precio=float(input("Cual es el precio del producto: "))
 cantidad=int(input("Cual es la cantidad del producto: "))
@@ -16,7 +18,9 @@ print("precio unitario: " , precio)
 print("cantidad: ", cantidad)
 print("costo total: ",total)
 ("--------------------------------------------------------------------------------------------")
+
 print("********************************* TALLER 2 ********************************")
+
 no1=input("Cual es el nombre del primer producto: ")
 no2=input("Cual es el nombre del segundo producto: ")
 no3=input("Cual es el nombre del tercer producto: ")
@@ -55,3 +59,82 @@ print("producto: " , nombre)
 print("precio unitario: " , precio)
 print("cantidad: ", cantidad)
 print("costo total: ",total)
+print("---------------------------------------------------------------------------------")
+
+print("**************************************** TALLER 3 ****************************************")
+print("-------------------------------------------------------------------------------------------")
+
+print("***************************************** REGISTRO DE NOTAS DE UN ESTUDIATE *****************************************")
+
+print("En este sistema se le pedira su nombre de usuario, nombre de las asignaturas, notas y al final se le dara su boletin")
+print("NOMBRE DE USIARIO")
+
+nombre=input("Ingrese su nombre")
+
+print("NOMBRE DE LAS ASIGNATURAS")
+
+asignatura1=input("ingrse el nombre de la asignatura")
+asignatura2=input("ingrse el nombre de la asignatura")
+asignatura3=input("ingrse el nombre de la asignatura")
+
+print("NOTA DE CADA ASIGNATURA")
+print(asignatura1)
+nota1=float(input("ingress la primera nota de la asignatura de",asignatura1,": "))
+nota2=float(input("ingress la segunda nota de la asignatura de",asignatura1,": "))
+promedio1=(nota1+nota2)/2
+
+
+print(asignatura2)
+nota3=float(input("ingress la primera nota de la asignatura de",asignatura2,": "))
+nota4=float(input("ingress la segunda nota de la asignatura de",asignatura2,": "))
+promedio2=(nota3+nota4)/2
+
+
+print(asignatura3)
+nota5=float(input("ingress la primera nota de la asignatura de",asignatura3,": "))
+nota6=float(input("ingress la segunda nota de la asignatura de",asignatura3,": "))
+promedio3=(nota5+nota6)/2
+
+print("--------------------------------------------------------------------")
+
+tupla1=(asignatura1,promedio1)
+tupla2=(asignatura2,promedio2)
+tupla3=(asignatura3,promedio3)
+
+print("-----------------------------------------------------------------")
+
+lita=[asignatura1,nota1,nota2]
+lita=[asignatura2,nota3,nota4]
+lita=[asignatura3,nota5,nota6]
+
+print("---------------------------------------------------------------------")
+
+diccionario={ "nombre": nombre,
+    "materias":asignatura1,
+    "materias":asignatura2,
+    "materias":asignatura3
+}
+
+promedio=(promedio1+promedio2+promedio3)/3
+
+print("-----------------------------------------------------------------------")
+print("*********************************BOLETIN*******************************")
+print("nombre del estudiante es:",nombre)
+print(asignatura1,promedio1)
+print(asignatura2,promedio2)
+print(asignatura3,promedio3)
+print("el promedio del estudiante es:",promedio)
+print("***********************************************************************")
+
+
+
+
+
+
+
+
+
+
+
+
+
