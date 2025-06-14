b07a212f3e7c94b0b996f8d989fd87be626a83d2
Melani sofia <mel251343@gmail.com>
2025-06-14 17:45:01 -0500

    while

---
+++ while.py
@@ -0,0 +1,14 @@
+contador=1
+while contador  <= 100:
+    print(f"contador:,{contador}")
+    contador +=1 #no poner - porfa 
+print("------------------------------------------------------------------------")
+while True:
+    texto=input("escribe algo(o escribe 'salir' para terminar): ")
+    if texto =="salir":
+        break
+    print("escribiste:",texto)
+
+
+
+
